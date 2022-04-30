799d0e2c5bcd0d6c0fdad57ea8c7940f638e4bac
Brendan <brendanw207207@csu.fullerton.edu>
2022-04-30 15:59:33 -0700

    Uploaded Test Code

---
+++ main.py
@@ -0,0 +1,20 @@
+# CPSC-440 Project
+# Brendan Wang and Katelyn Choi
+
+import adafruit_dht
+import time
+import board
+
+dhtSensor = adafruit_dht.DHT22(board.D4)
+
+while True:
+    try:
+        temp_c = dhtSensor.temperature
+    except RuntimeError:
+        print("An error has occured, trying again.")
+        continue
+
+    print("The temperature is currently ", end="")
+    print(temp_c, end="")
+    print(" Celcius.")
+    time.sleep(60)
